133b59acb129334bce769b455d2a5d4163ac7896
RayyaneElmalhouni <elmalhounirayyane@gmail.com>
2022-03-24 19:54:33 +0100

    Creating and Linking Components

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+   import Controller from "./components/Controller.svelte";
+	import Result from "./components/Result.svelte";
+</script>
+
+<h1>Text Editor</h1>
+<Controller />
+<Result />
